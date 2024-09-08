b32c7fcf0e15407b05882e63676669f5ca20a475
Xarasho <alex.olmedo.rojas@gmail.com>
2024-09-08 11:54:52 -0600

    Added DotProduct function

--- main.go
+++ main.go
@@ -10,9 +10,19 @@ type Neuron struct {
   bias    float64
 }
 
+func DotProduct(vector1 []float64, vector2 []float64) float64 {
+  output := 0.0 
+
+// dot product between two vectors
+    for k := 0; k < len(vector1); k++ {
+      output += vector1[k] * vector2[k]
+    }
+  return output
+}
+
 func main() {
 
-	inputs := [4]float64{1, 2, 3, 2.5}
+	inputs := []float64{1, 2, 3, 2.5}
 
   n1 := Neuron{[]float64{0.2, 0.8, -0.5, 1.0}, 2.0}
   n2 := Neuron{[]float64{0.5, -0.91, 0.26, -0.5}, 3}
@@ -23,13 +33,7 @@ func main() {
 
   for i := 0; i < len(neurons); i++ {
     neuron := neurons[i]
-    output := 0.0 
-
-    for k := 0; k < len(inputs); k++ {
-      output += inputs[k] * neuron.weights[k]
-    }
-    output += neuron.bias
-
+    output := DotProduct(inputs, neuron.weights) + neuron.bias
     outputs = append(outputs, output)
   } 
 
